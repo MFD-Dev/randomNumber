de5ca63fd22398c4961d3ca234acf9f56cfefccc
Michael <Michaelflowersdev@gmail.com>
2021-06-29 11:12:30 -0500

    initial commit

---
+++ app.js
@@ -0,0 +1,28 @@
+/*
+-- create variables to gues the number range
+-- variable to hold the answer value
+-- form for the user to enter information
+-- function to calculate and return value
+-- prompts to enter information and confirm if correct
+-- alert to show it is correct
+*/
+
+
+var userEntry = document.getElementById('numberGuess');
+var subBtn = document.getElementsByTagName('submit');
+var rdmNum = document.getElementById('random_number').innerHTML = Math.random();
+
+console.log(subBtn)
+userEntry.addEventListener('click', () => {
+    console.log('I hear it');
+})
+
+var noShow = function () {
+    n
+}
+
+
+
+subBtn.addEventListener('click', () => {
+    console.log('btn');
+})
