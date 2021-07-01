4067c237cdde97c4183accc4bdab5871251b15c4
Michael <Michaelflowersdev@gmail.com>
2021-07-01 10:04:20 -0500

    first mobile commit

--- app.js
+++ app.js
@@ -1,28 +1,67 @@
-/*
--- create variables to gues the number range
--- variable to hold the answer value
--- form for the user to enter information
--- function to calculate and return value
--- prompts to enter information and confirm if correct
--- alert to show it is correct
+/*  use the Math.random() method to create a function that will return a random integer between two values (inclusive).
+
 */
+ 
+// varial to access grab submit btn.
+const subBtn = document.querySelector("#subBtn");
+
+// onClick generate a random number.
+subBtn.addEventListener("click", generateRandomNum);
 
 
-var userEntry = document.getElementById('numberGuess');
-var subBtn = document.getElementsByTagName('submit');
-var rdmNum = document.getElementById('random_number').innerHTML = Math.random();
+function generateRandomNum() {
+  // take the values from the input field
+  // const fromNum = document.querySelector("#fromNum");
+  // const toNum = document.querySelector("#toNum");
 
-console.log(subBtn)
-userEntry.addEventListener('click', () => {
-    console.log('I hear it');
-})
-
-var noShow = function () {
-    n
+  const getRandomNumber = (min, max) => {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+    console.log(getRandomNumber)
+  }
+  result =  getRandomNumber(1, 100);
+  console.log(result)
 }
 
+// HTML:
+// -----------
+// <p>Text 1: <input id="text1" type="text"></p>
+        
+// <p>Text 2: <input id="text2" type="text"></p>
+        
+// <p><button onclick="compareText()">Compare</button></p>
+
+// function compareText() {
+//   var text1 = document.getElementById("text1").value;
+//   var text2 = document.getElementById("text2").value;
+  
+//   if (text1 == text2) {
+//       alert("Same Text!");
+//   } else {
+//       alert("Different Texts.");
+//   }
+// }
 
 
-subBtn.addEventListener('click', () => {
-    console.log('btn');
-})
+
+
+
+
+
+
+// subBtn.addEventListener("click", getRandomNumber)
+
+// function getRandomNumber () {
+//   alert('Hello');
+// }
+// const getRandomNumber = (min, max) => {
+//     return Math.floor(Math.random() * (max - min + 1)) + min;
+//   };
+
+//   const rollDice = () => getRandomNumber(1, 100);
+
+// document.getElementById("random_number");
+// //   document.getElementById("subBtn").addEventListener("click", function () {
+      
+// //     console.log(getRandomNumber);
+
+// }
